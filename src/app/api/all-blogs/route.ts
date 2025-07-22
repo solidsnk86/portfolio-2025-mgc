b2b1377b697e7acf770e3ede628e58c7c68229a1
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-07-22 02:56:00 -0300

    fix api

--- src/app/api/all-blogs/route.ts
+++ src/app/api/all-blogs/route.ts
@@ -13,8 +13,8 @@ export async function GET() {
           { encoding: "utf-8" }
         );
         if (allBlogContent) {
-          const titles = allBlogContent.split("\r\n")[2];
-          const dates = allBlogContent.split("\r\n")[0];
+          const titles = allBlogContent.split(/\r?\n/)[2].replace("#", "");
+          const dates = allBlogContent.split(/\r?\n/)[0];
           const fileName = file.replace(".md", "");
           blogArray.push({ name: fileName, title: titles, date: dates });
         }
