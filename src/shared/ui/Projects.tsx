c0d56be08905e6e8a63bd11a3a53e50a96f0a280
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-07-22 17:43:56 -0300

    style: updated hover scale

--- src/shared/ui/Projects.tsx
+++ src/shared/ui/Projects.tsx
@@ -64,7 +64,7 @@ export const Projects = () => {
           <Link
             key={name}
             href={`/projects/${url}`}
-            className="p-4 rounded-xl bg-[var(--card-project-color)] grid mx-auto w-full justify-center space-y-3 relative overflow-hidden group hover:scale-105 transition-transform duration-300 hover:shadow-2xl"
+            className="p-4 rounded-xl bg-[var(--card-project-color)] grid mx-auto w-full justify-center space-y-3 relative overflow-hidden group hover:scale-102 transition-transform duration-300 hover:shadow-2xl"
           >
             <span className="p-1 rounded-xl bg-[var(--icon-bg)] group-hover:scale-90 group-hover:translate-y-3 transition-transform duration-300">
             <Image
