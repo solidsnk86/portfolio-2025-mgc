651d8794f274404588f57772144795f3f8c8f7f2
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-07-22 23:36:58 -0300

    updated meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
       <ThemeWrapper>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
