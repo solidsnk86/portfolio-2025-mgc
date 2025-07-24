569786be7bab99344942c7572988d944cf93daa4
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-07-24 14:01:31 -0300

    updated styles btn

--- src/shared/ui/ScrollToTop.tsx
+++ src/shared/ui/ScrollToTop.tsx
@@ -1,15 +1,16 @@
 import { ArrowUpToLine } from "lucide-react";
 import { useEffect, useState } from "react";
+import styles from "../styles/scroll.module.css";
 
 export const ScrollToTopButton = () => {
-  const [show, setShow] = useState<boolean>();
+  const [show, setShow] = useState<boolean>(false);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 400) {
         setShow(true);
       } else {
-        setShow(false);
+        setShow(false)
       }
     });
   }, []);
@@ -21,11 +22,13 @@ export const ScrollToTopButton = () => {
       behavior: "smooth",
     });
   };
+
   return (
     show && (
       <button
         onClick={scrollToTop}
-        className="fixed bottom-10 left-6 p-2 rounded-lg border border-[var(--border-color)] z-50 bg-[var(--header-bg-color)] cursor-pointer shadow-md"
+        className={`fixed bottom-10 left-6 p-2 rounded-lg border border-[var(--border-color)] 
+            z-50 bg-[var(--header-bg-color)] cursor-pointer shadow-md ${styles.scroll}`}
       >
         <ArrowUpToLine size={20} />
       </button>
