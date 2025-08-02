b5bc49b633f9c3de28d925d1beb2c0c6df60caa1
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-08-02 14:09:22 -0300

    updated redirect

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,15 @@
+import { redirect } from "next/navigation";
 import { ClientAdminPage } from "./ClientAdminPage";
 
+const OWN_IP = process.env.DESKTOP_IP
+
 export default async function AdminPage() {
+  const { ip } = await fetch("https://solid-geolocation.vercel.app/location")
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => console.log(err));
+
+  if (ip !== OWN_IP) redirect("https://neo-wifi.vercel.app");
   return (
     <div className="h-full flex flex-col z-[999]">
       <header className="p-6 border-b border-[var(--border-color)] bg-[var(--header-bg-color)]">
