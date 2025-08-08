dc72f1301ad2b0fcc7c177cc4190f4b5efb46674
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-08-07 21:23:13 -0300

    remove relative

--- src/shared/ui/Card.tsx
+++ src/shared/ui/Card.tsx
@@ -16,7 +16,7 @@ export const Card = ({
   text: string;
 }) => {
   return (
-    <div className={`xl:p-16 md:p-10 p-8 mx-2 rounded-xl colder text-center flex flex-col justify-center relative text-zinc-100 ${poppins.className} overflow-x-hidden`}>
+    <div className={`xl:p-16 md:p-10 p-8 mx-2 rounded-xl colder text-center flex flex-col justify-center text-zinc-100 ${poppins.className} overflow-x-hidden`}>
       <h2 className="xl:text-4xl text-2xl font-semibold xl:flex flex flex-col mx-auto gap-2 items-center justify-center">
         {children}
         {title}
