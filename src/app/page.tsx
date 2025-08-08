37cb7fb4d5d379482c04335cc2575715d7bf65a8
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-08-07 21:20:44 -0300

    style: updated responsive card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import { Card } from "@/shared/ui/Card";
 import { Projects } from "@/shared/ui/Projects";
 import { Blogsito } from "@/shared/ui/Blogsito";
 import { ScrollToTopButton } from "@/shared/ui/ScrollToTop";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -25,9 +26,12 @@ export default function Home() {
       <Blogsito />
       <Dots className="flex justify-center mx-auto my-16" />
       <Card title="Tenés algo en mente?" text="Lo hacemos realidad..">
-        <picture>
-          <img src="/assets/fireball.png" alt="Fireball draw" className="xl:w-[180px] xl:h-[120px] w-[80px] h-[60px]" />
-        </picture>
+        <Image
+          src="/assets/fireball.png"
+          width={180}
+          height={120}
+          alt="Fireball draw"
+        />
       </Card>
       <Dots className="flex justify-center mx-auto my-16" />
       <ContactForm />
--- src/shared/ui/Card.tsx
+++ src/shared/ui/Card.tsx
@@ -16,8 +16,8 @@ export const Card = ({
   text: string;
 }) => {
   return (
-    <div className={`xl:p-16 md:p-10 p-8 mx-2 rounded-xl colder text-center flex flex-col justify-center text-zinc-100 ${poppins.className} overflow-x-hidden`}>
-      <h2 className="xl:text-4xl text-2xl font-semibold flex gap-2 items-center justify-center">
+    <div className={`xl:p-16 md:p-10 p-8 mx-2 rounded-xl colder text-center flex flex-col justify-center relative text-zinc-100 ${poppins.className} overflow-x-hidden`}>
+      <h2 className="xl:text-4xl text-2xl font-semibold xl:flex flex flex-col mx-auto gap-2 items-center justify-center">
         {children}
         {title}
       </h2>
