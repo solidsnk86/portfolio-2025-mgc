cdc54fd9fa6adae22dec8427056e2078c06bac07
solidsnk86 <calcagni.gabriel86@gmail.com>
2026-05-01 07:21:09 -0300

    feat: agregar soporte para entornos de desarrollo en la configuración del cliente Supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const suapabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
+  isDev ? process.env.NEXT_PUBLIC_SUPABASE_URL! : process.env.SUPABASE_URL!,
+  isDev ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! : process.env.SUPABASE_ANON_KEY!
 );
 
 export { suapabase };
